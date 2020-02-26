ae367ca636429a845479d6232cf069acad136614
lsm32333 <lsm32333@naver.com>
2020-02-26 20:18:36 +0900

    initial commit

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
-const PORT = 4000;
+const PORT = 4001;
 
-function handleListening() {
-  console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleHome(req, res) {
-    res.send("Hello from home");
-  }
+const handleHome = (req, res) => res.send("Hello from my ass");
   
-  function handleProfile(req, res) {
-    res.send("You are on my profile");
-  }
+const handleProfile = (req, res) => res.send("You are on my profile");
   
-  app.get("/", handleHome);
+app.get("/", handleHome);
   
-  app.get("/profile", handleProfile);
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
